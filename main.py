90476cb4f9f3dd6d630110815d7c9c0bc6275297
joelcruz8 <123519375+joelcruz8@users.noreply.github.com>
2025-01-25 17:15:55 -0500

    added model and routes

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ def create_app():
 
     # Register blueprints from each module
     from ConflictionWarnings.routes import blueprint as conflict_blueprint
+    from pillModel.routes import blueprint as pillModel
     #from PillRecognition.routes import blueprint as pill_recognition_blueprint
     #from ChatBot.routes import blueprint as chatbot_blueprint
 
     app.register_blueprint(conflict_blueprint, url_prefix='/conflict')
     #app.register_blueprint(pill_recognition_blueprint, url_prefix='/pill')
     #app.register_blueprint(chatbot_blueprint, url_prefix='/chatbot')
+
+    app.register_blueprint(pillModel, url_prefix='/predict')
 
     return app
 
